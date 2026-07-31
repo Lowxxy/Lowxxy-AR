3011f81bcf8d6e967f54feab7b2a99bb45bc4e30
Lowxxy <lowxxybrand@gmail.com>
2026-07-31 18:29:42 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lowxxy-ar-v49-four-designs-both-popart-medium';
+const CACHE_NAME = 'lowxxy-ar-v50-four-designs-both-popart-huge';
 const APP_SHELL = [
   './',
   './index.html',
