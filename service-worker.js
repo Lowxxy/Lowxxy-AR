bdf5eac89bf66028fa2992fe7cbf158ccc890375
Lowxxy <lowxxybrand@gmail.com>
2026-08-01 01:08:54 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lowxxy-ar-full-v101-popart-reset-sticky-hang7-fit';
+const CACHE_NAME = 'lowxxy-ar-full-v103-popart-reset-sticky-hang7-fit';
 const APP_SHELL = [
   './',
   './index.html',
